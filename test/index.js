f31e8806a586d1069753ba7dc0f8476c4d1eae75
Ian Macalinao <ian@giza.us>
2014-07-20 21:42:11 -0500

    Remove bad tests
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- test/index.js
+++ test/index.js
@@ -2,8 +2,4 @@ var expect = require('chai').expect,
     mongooseRestifier = require('..');
 
 describe('mongoose-restifier', function() {
-  it('should say hello', function(done) {
-    expect(mongooseRestifier()).to.equal('Hello, world');
-    done();
-  });
 });
