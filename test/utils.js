b1524370f97a6a1c938a08a2aaed213223b7b60b
Ian Macalinao <ian@giza.us>
2014-07-20 20:11:19 -0500

    Add tests for utils
    Signed-off-by: Ian Macalinao <ian@giza.us>

---
+++ test/utils.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var expect = require('chai').expect;
+var utils = require('../lib/utils');
+
+describe('utils', function() {
+  describe('parseFilterString', function() {
+    it('should break up a filter string into parts', function() {
+      var res = utils.parseFilterString('find a | destroy b | kill');
+      expect(res).to.eql([
+        ['find', 'a'],
+        ['destroy', 'b'],
+        ['kill']
+      ]);
+    });
+    it('should tolerate double quotes', function() {
+      var res = utils.parseFilterString('find "a license to kill" | destroy b | kill');
+      expect(res).to.eql([
+        ['find', '"a license to kill"'],
+        ['destroy', 'b'],
+        ['kill']
+      ]);
+    });
+    it('should tolerate single quotes', function() {
+      var res = utils.parseFilterString("find 'a license to kill' | destroy b | kill");
+      expect(res).to.eql([
+        ['find', "'a license to kill'"],
+        ['destroy', 'b'],
+        ['kill']
+      ]);
+    });
+  });
+});
