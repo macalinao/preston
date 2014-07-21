31a9c4c368a7d049b78f5e66aa9c4c1a50264455
Ian Macalinao <me@ian.pw>
2014-07-21 12:16:23 -0500

    Remove the #serve describer, as we're doing bdd
    Signed-off-by: Ian Macalinao <me@ian.pw>

--- test/model.js
+++ test/model.js
@@ -25,48 +25,46 @@ describe('Model', function() {
     expect(UserModel.restricted).to.eql(['password']);
   });
 
-  describe('#serve', function() {
-    describe('query', function() {
-      it('should return all documents with query', function(done) {
-        request(app).get('/users').end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res.body.length).to.equal(5);
-          done();
-        });
+  describe('query', function() {
+    it('should return all documents with query', function(done) {
+      request(app).get('/users').end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.body.length).to.equal(5);
+        done();
       });
+    });
 
-      it('should return a specific document when a param is given', function(done) {
-        request(app).get('/users').query({
-          name: 'Bob'
+    it('should return a specific document when a param is given', function(done) {
+      request(app).get('/users').query({
+        name: 'Bob'
+      }).end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.body.length).to.equal(1);
+        expect(res.body[0].name).to.equal('Bob');
+        done();
+      });
+    });
+
+    it('should not return restricted fields', function(done) {
+      request(app).get('/users').query({
+        name: 'Bob'
+      }).end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.body[0].password).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should prevent searching for restricted fields', function(done) {
+      request(app).get('/users')
+        .query({
+          password: 'hunter2'
         }).end(function(err, res) {
           expect(err).to.be.null;
-          expect(res.body.length).to.equal(1);
-          expect(res.body[0].name).to.equal('Bob');
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.match(/Cannot access restricted field/);
           done();
         });
-      });
-
-      it('should not return restricted fields', function(done) {
-        request(app).get('/users').query({
-          name: 'Bob'
-        }).end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res.body[0].password).to.be.undefined;
-          done();
-        });
-      });
-
-      it('should prevent searching for restricted fields', function(done) {
-        request(app).get('/users')
-          .query({
-            password: 'hunter2'
-          }).end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res.status).to.equal(401);
-            expect(res.body.message).to.match(/Cannot access restricted field/);
-            done();
-          });
-      });
     });
   });
 
